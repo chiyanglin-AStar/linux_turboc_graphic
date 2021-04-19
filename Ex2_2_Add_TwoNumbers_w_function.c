deddce9946791fedc762d84e390bd830f0360452
chiyang <chiyanglin@gmail.com>
2021-04-19 10:08:12 +0800

    make example orderly

---
+++ Ex2_2_Add_TwoNumbers_w_function.c
@@ -0,0 +1,13 @@
+// Add Two Number Version 2
+
+#include <stdio.h>
+int main()
+{
+ int a, b;
+ printf("A=> ");
+ scanf("%d", &a);
+ printf("B=> ");
+ scanf("%d", &b);
+ printf("A+B=%d\n", a+b);
+ return 0;
+}
